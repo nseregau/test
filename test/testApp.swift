6da82c1424489af8d4a9dddae3f217715b6cd497
Sergii Nesterenko <nseregau@gmail.com>
2025-09-10 02:17:29 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Sergii Nesterenko on 10.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
